73f18e643efdfd2360deb577b463da5e29ef921a
fuxiang <449585662@qq.com>
2021-04-02 14:01:37 +0800

    fix test case

--- test/unit/webpack.base.js
+++ test/unit/webpack.base.js
@@ -4,7 +4,7 @@ const path = require('path');
 describe('builder-webpack test case', () => {
     const baseConfig = require('../../lib/webpack.base');
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, 'D:/code/practice/frontEnd/webpack-study/builder-webpack/test/smoke/template/src/index/index.js')
-        assert.equal(baseConfig.entry.search, 'D:/code/practice/frontEnd/webpack-study/builder-webpack/test/smoke/template/src/search/index.js')
+        assert.equal(baseConfig.entry.index.indexOf('test/smoke/template/src/index/index.js') > -1, true);
+        assert.equal(baseConfig.entry.search.indexOf('test/smoke/template/src/search/index.js') > -1, true)
     });
 })
