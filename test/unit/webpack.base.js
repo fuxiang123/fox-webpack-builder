fd851399545bd5f0c8865519debcbbe091996d12
fuxiang <449585662@qq.com>
2021-04-02 13:55:23 +0800

    fix nycrc and eslintrc

--- test/unit/webpack.base.js
+++ test/unit/webpack.base.js
@@ -3,7 +3,6 @@ const path = require('path');
 
 describe('builder-webpack test case', () => {
     const baseConfig = require('../../lib/webpack.base');
-    console.log(baseConfig);
     it('entry', () => {
         assert.equal(baseConfig.entry.index, 'D:/code/practice/frontEnd/webpack-study/builder-webpack/test/smoke/template/src/index/index.js')
         assert.equal(baseConfig.entry.search, 'D:/code/practice/frontEnd/webpack-study/builder-webpack/test/smoke/template/src/search/index.js')
